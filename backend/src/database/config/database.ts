55e2b022ddbcb0e60238fa8906c2f04984b7a9f0
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-08 10:24:13 -0300

    fix: alterado var de ambiente para DB_PORT

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -3,7 +3,7 @@ import { Options } from 'sequelize';
 
 const config:Options = {
   dialect:'mysql',
-  port: Number(process.env.PORT) || 3001,
+  port: Number(process.env.DB_PORT) || 3001,
   host: process.env.DB_HOST || 'localhost',
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || 'secret',
