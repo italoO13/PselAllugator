aa481af5b7d30f8a575e765c1bbb35d2f2c37fce
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-09 19:48:04 -0300

    feat: camada de route para o metodo getAll na rota products

---
+++ backend/src/routes/productsRouter.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import ProductsModel from "../repository/products/ProductsModel";
+import ProductsService from "../services/products/ProductsService";
+import ProductsController from "../controllers/products/ProductsController";
+
+const productsModel = new ProductsModel();
+const productsService = new ProductsService(productsModel);
+const productsController = new ProductsController(productsService)
+
+const router = Router();
+
+router.get('/', productsController.getAll)
+
+export default router;
