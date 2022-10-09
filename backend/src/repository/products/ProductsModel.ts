b121cdc47f73559a4fa01af3f4421d3a52432fd3
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-09 19:45:25 -0300

    feat: implementado metodo getAll que retorna produtos e qtd em estoque

---
+++ backend/src/repository/products/ProductsModel.ts
@@ -0,0 +1,45 @@
+import ProductInventory from "../../database/models/ProductInventory";
+import Product from "../../database/models/Product";
+import IProduct from "../../interfaces/IProduct";
+import IProductsModel from "./IProductsModel";
+import Subscription from "../../database/models/Subscription";
+import IProductInventory from "../../interfaces/IProductInventory";
+
+
+export default class ProductsModel implements IProductsModel {
+  async getAll(): Promise<IProduct[]> {
+    const result = await Product.findAll({
+      include: [
+        {model: ProductInventory, as: 'inventory', 
+        include: [{model: Subscription, as:'subscriptions'}],
+        attributes: {exclude: ['productId']}}
+      ]
+    });
+
+    const newResult = this.adapter(result);
+    return newResult
+  }
+
+  private adapter(products:IProduct[]):IProduct[] {
+    const newProduct = products.map((prod) => {
+      const {id, name, description, image, price, categoryId}:IProduct = prod
+      if(!prod.inventory) {
+        return {id, name, description, image, price, categoryId, qtd:0}
+      }
+      return {id, name, description, image, price, categoryId, qtd: this.countQtd(prod.inventory)}
+    })
+    return newProduct;
+  }
+
+  private countQtd(inventory: IProductInventory[]):number {
+    return inventory.reduce((acc:number, invent:IProductInventory) => {
+      if(invent.subscriptions.length === 0) {
+        return acc + 1
+      }
+      if(invent.subscriptions.every(({devolution}) => devolution===true )) {
+        return acc +1
+      }
+      return acc
+    }, 0)
+  }
+}
