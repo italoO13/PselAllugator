196e5002d0fbbce48dabaa35624a97d0b49f998d
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-10 20:11:21 -0300

    feat: camada de controller do metodo login da rota session

---
+++ backend/src/controllers/session/SessionController.ts
@@ -0,0 +1,27 @@
+import ISessionController from "./ISessionController";
+import {Request, Response, NextFunction} from 'express'
+import ISessionService from "../../services/session/ISessionService";
+import Auth from "../../helper/Auth";
+
+export default class SessionController implements ISessionController {
+
+  private service: ISessionService;
+  private auth: Auth
+
+  constructor(service:ISessionService){
+    this.service = service;
+    this.auth = new Auth();
+  }
+
+ login = async(req:Request, res:Response, next:NextFunction) => {
+  try {
+    const {email, password} = req.body;
+    const id = await this.service.login(email, password)
+    const token = this.auth.generateToken({id})
+    res.status(200).json({token})
+  } catch (error) {
+    next(error);
+  }
+ }
+
+}
