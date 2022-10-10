0a122e5eb6e009908bfd5c52099f1272c8f5d64d
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-10 18:18:39 -0300

    refactor: alterado para senha criptografada ao criar um novo usuario

--- backend/src/services/clients/ClientsService.ts
+++ backend/src/services/clients/ClientsService.ts
@@ -1,6 +1,7 @@
 import IClient from "../../interfaces/IClient";
 import IClientsService from "./IClientsService";
 import IClientsModel from "../../repository/clients/IClientsModel";
+import crypt from "../../helper/crypt";
 
 export default class ClientsService implements IClientsService {
   private model: IClientsModel
@@ -10,7 +11,7 @@ export default class ClientsService implements IClientsService {
   }
 
   async create(client: IClient): Promise<void> {
-    await this.model.create(client);
+    await this.model.create({...client, password: crypt.encryptPassword(client.password)});
   }
   
 }
