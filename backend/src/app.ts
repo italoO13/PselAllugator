42ae7ce079c961e875c84e99b0b19eb5ef87e4c2
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-10 11:24:44 -0300

    refactor: alterando localizacao do middleware de tratamento de erros

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,6 @@ class App {
     this.config()
     this.app.use('/', router);
     this.app.get('/hello', (req, res) => res.send('word'))
-    this.middlewareError()
   }
 
   private config():void{
@@ -18,14 +17,6 @@ class App {
   public start(PORT: string | number):void {
     this.app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
   }
-
-  private middlewareError() {
-    this.app.use((error:Error,req:express.Request, res:express.Response, _next:express.NextFunction) => {
-      console.log(error);
-      res.status(500).json({message: 'Internal error server'})
-    })
-  }
-
 }
 
 export default App;
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -5,5 +5,12 @@ const router = Router();
 
 router.use('/products', productsRouter)
 
+router.use((error:CustomError,req:Request, res:Response, next:NextFunction) => {
+  console.log(error);
+  if(error.code){
+    return res.status(error.code).json({message: error.message})
+  }
+  res.status(500).json({message: 'Internal error server'})
+})
 
 export default router;
