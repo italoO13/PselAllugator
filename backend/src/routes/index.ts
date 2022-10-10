81d76bf1e49a4224b665552172e4a82a65963980
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-10 11:28:13 -0300

    feat: adicionado rota Client e seu metodo Post

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import { Request, Response, NextFunction } from "express";
+import CustomError from "../helper/CustomError";
 import productsRouter from './productsRouter'
+import clientsRouter from './clientsRouter';
 
 const router = Router();
 
 router.use('/products', productsRouter)
+router.use('/clients', clientsRouter);
 
 router.use((error:CustomError,req:Request, res:Response, next:NextFunction) => {
   console.log(error);
