93f445a6d030d8644c5dea0dd86a6336ec3f4adb
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-10 08:37:55 -0300

    chore: mock de products e productsAdapter

---
+++ backend/src/tests/mocks/index.ts
@@ -0,0 +1,91 @@
+export default {
+
+  products: [
+      {
+        "id": 1,
+        "name": "Smartphone Motorola Moto G22",
+        "description": "128GB 4G Wi-Fi Tela 6.5 Dual Chip 4GB RAM Câmera Quádrupla + Selfie 16MP",
+        "image": "https://images-americanas.b2w.io/produtos/01/00/img/4818919/7/4818919742_1SZ.jpg",
+        "price": "1193.94",
+        "categoryId": 1,
+        "inventory": [
+          {
+            "id": 1,
+            "subscriptions": [
+              {
+                "devolution": false
+              }
+            ]
+          },
+          {
+            "id": 2,
+            "subscriptions": [
+              {
+                "devolution": true
+              },
+              {
+                "devolution": false
+              }
+            ]
+          },
+          {
+            "id": 5,
+            "subscriptions": []
+          },
+          {
+            "id": 9,
+            "subscriptions": []
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Smartphone xiaomi redmi note 11 pro",
+        "description": "/ tela 6,67 / 6GB / 128GB / 5G / dual chip",
+        "image": "https://images-americanas.b2w.io/produtos/5931011752/imagens/smartphone-xiaomi-redmi-note-11-pro-azul-tela-6-67-6gb-128gb-5g-dual-chip-preto/5931011779_1_xlarge.jpg",
+        "price": "2500.94",
+        "categoryId": 2,
+        "inventory": [
+          {
+            "id": 3,
+            "subscriptions": [
+              {
+                "devolution": true
+              }
+            ]
+          },
+          {
+            "id": 4,
+            "subscriptions": []
+          },
+          {
+            "id": 8,
+            "subscriptions": []
+          }
+        ]
+      },
+  ],
+  productsAdapted: [
+    {
+      "id": 1,
+      "name": "Smartphone Motorola Moto G22",
+      "description": "128GB 4G Wi-Fi Tela 6.5 Dual Chip 4GB RAM Câmera Quádrupla + Selfie 16MP",
+      "image": "https://images-americanas.b2w.io/produtos/01/00/img/4818919/7/4818919742_1SZ.jpg",
+      "price": "1193.94",
+      "categoryId": 1,
+      "qtd": 2
+    },
+  {
+      "id": 2,
+      "name": "Smartphone xiaomi redmi note 11 pro",
+      "description": "/ tela 6,67 / 6GB / 128GB / 5G / dual chip",
+      "image": "https://images-americanas.b2w.io/produtos/5931011752/imagens/smartphone-xiaomi-redmi-note-11-pro-azul-tela-6-67-6gb-128gb-5g-dual-chip-preto/5931011779_1_xlarge.jpg",
+      "price": "2500.94",
+      "categoryId": 2,
+      "qtd": 3
+    },
+  
+  ]
+
+
+}
