13294383d34cf54abba3d3fbb608e28e84ab4529
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-10 20:10:45 -0300

    feat: helper com funções relacionadas a authenticação

---
+++ backend/src/helper/Auth.ts
@@ -0,0 +1,24 @@
+import * as jwt from 'jsonwebtoken';
+import 'dotenv/config';
+
+export default class Auth {
+  private config: jwt.SignOptions;
+  private secret: jwt.Secret;
+
+  constructor() {
+    this.config = {
+      expiresIn:'1d',
+      algorithm:'HS256'
+    };
+    this.secret = process.env.JWT_SECRET || 'secret';
+  }
+
+  generateToken = (body:object):string => {
+    const token = jwt.sign(
+      {...body}, this.secret, {expiresIn: this.config.expiresIn, algorithm: this.config.algorithm}
+    )
+    return token;
+  }
+
+
+}
