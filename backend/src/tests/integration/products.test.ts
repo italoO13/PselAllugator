6f1743ac8d8ca1f0d3a04081327dfe4025f2aac2
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-10 08:38:37 -0300

    test: verifica se é possível retornar os produtos utilizando o metodo getAll

---
+++ backend/src/tests/integration/products.test.ts
@@ -0,0 +1,35 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+
+import chaiHttp = require('chai-http');
+
+import mock from '../mocks';
+import { app } from '../../app';
+import Product from '../../database/models/Product'
+chai.use(chaiHttp);
+const {expect} = chai;
+
+describe('Testa a rota Products', () => {
+  describe('Quando é solicitado todos os produtos disponíveis pela rota GET', () => {
+    beforeEach(() => {
+      sinon.stub(Product, 'findAll').resolves(mock.products as unknown as Product[]);
+    })
+    afterEach(() => {
+      sinon.restore()
+    })
+
+    it('deve retornar status igual a 200', async () => {
+      const result = await chai.request(app).get('/products');
+      expect(result.status).to.be.equal(200);
+    })
+
+    it('deve retornar um array de objetos semelhante ao mock.products', async () => {
+      const result = await chai.request(app).get('/products');
+      expect(result.body).to.deep.equal(mock.productsAdapted);
+    })
+
+
+  })
+
+
+})
