19f0f176d6fa8b81f4d6e310bdc0f8378b43e383
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-11 14:00:13 -0300

    refactor: inserido tipo para req.client

--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -2,6 +2,7 @@ import Auth from "../helper/Auth";
 import {Response, NextFunction} from 'express'
 import CustomError from "../helper/CustomError";
 import IRequestWithClient from "../interfaces/IRequestWithClient";
+import IJWT from "../interfaces/IAuth";
 
 export default class AuthMiddleware {
   private auth: Auth;
@@ -16,7 +17,7 @@ export default class AuthMiddleware {
     }
     try {
       const client = await this.auth.decodeToken(authorization)
-      req.client = client;
+      req.client = client as IJWT;
       next()
     } catch (error) {
       next(new CustomError(403, 'Access token invalid'))
