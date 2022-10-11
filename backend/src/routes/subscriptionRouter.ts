1e6484d8ef9b9b6f0dc43726ee933fea338dccb3
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-11 14:00:32 -0300

    feat: adicionado rota subscription para o metodo Get

---
+++ backend/src/routes/subscriptionRouter.ts
@@ -0,0 +1,19 @@
+import SubscriptionModel from "../repository/subscription/SubscriptionModel";
+import SubscriptionService from "../services/subscription/SubscriptionService";
+import SubscriptionController from "../controllers/subscription/SubscriptionController";
+import AuthMiddleware from "../middlewares/auth";
+
+import { Router } from "express";
+
+const router = Router();
+
+const subscriptionModel = new SubscriptionModel()
+const subscriptionService = new SubscriptionService(subscriptionModel)
+const subscriptionController = new SubscriptionController(subscriptionService)
+const authMiddleware = new AuthMiddleware()
+
+
+router.get('/',authMiddleware.verifyClient, subscriptionController.getSubsByClient);
+
+export default router;
+
