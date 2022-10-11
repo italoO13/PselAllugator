e7c4122d071d02674573755b130d786feaaa9aac
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-11 13:57:54 -0300

    refactor: alterado password do seeder de client

--- backend/src/database/seeders/081020221421-clients.js
+++ backend/src/database/seeders/081020221421-clients.js
@@ -6,7 +6,7 @@ module.exports = {
         {
           name:'Italo',
           email: 'italo@gmail.com',
-          password:'secret',
+          password:'$2a$10$Elh/xNe4WqsH8qxlNjBT9.8lGNe7l81yzKNM9neoMRY9Rbf4BXKz6',
           cpf:'1145487855'
         },
       ]
