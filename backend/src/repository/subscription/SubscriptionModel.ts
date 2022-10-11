12b572ea998e100f1446263676da8a587618ac43
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-11 20:11:35 -0300

    refactor: alterado logica do método create para identificar um produto disponível no estoque

--- backend/src/repository/subscription/SubscriptionModel.ts
+++ backend/src/repository/subscription/SubscriptionModel.ts
@@ -5,11 +5,35 @@ import ProductInventory from "../../database/models/ProductInventory";
 import Product from "../../database/models/Product";
 
 export default class SubscriptionModel implements ISubscriptionModel {
-  async create(sub: ISubscription): Promise<void> {
-    await Subscription.create({...sub});
+
+  private async getProductInventory(id:number):Promise<ProductInventory[]> {
+    const result = await ProductInventory.findAll({
+      include:[
+        {model: Subscription, as:'subscriptions'}
+      ],
+      where: {productId: id}
+    }) 
+
+    return result.filter((obj: any) =>{ 
+      if(obj.subscriptions.length ===0) {
+        return true;
+      }
+
+      if(obj.subscriptions.some((sub:ISubscription) => sub.devolution === true)){
+        return true;
+      }
+    })
+  }
+
+
+  async create(sub: ISubscription, productId:number): Promise<void> {
+    const availableProducts = await this.getProductInventory(productId);
+
+    await Subscription.create({...sub, productInventId: availableProducts[0].id});
   }
 
   async getSubsById(id: number): Promise<ISubscription[]> {
+
     const result = await Subscription.findAll({
       include:[
         {model: ProductInventory, as: 'productInventory', include:[
