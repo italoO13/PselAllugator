bbd1cab0099256d195adb9f54a6f51ea0332e970
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-12 12:59:04 -0300

    refactor: add imagem do produto no card

--- frontend/src/components/CardProduct.jsx
+++ frontend/src/components/CardProduct.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function CardProduct({ prod }) {
-  const { id, name, price } = prod;
+  const {
+    id, name, price, image,
+  } = prod;
 
   return (
     <div key={id}>
+      <img src={image} alt={name} width="150px" />
       <h2>{name}</h2>
       <p>{`R$ ${price}`}</p>
       <button type="button">Ver produto</button>
