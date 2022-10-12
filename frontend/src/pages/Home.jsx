d5133fd0c2ec02dd9700b6f8e90094f47eb08df7
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-12 12:40:15 -0300

    feat: page de home com renderização de products

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import appContext from '../contexts/AppContext';
+import { getProducts } from '../api/products';
 
 function Home() {
+  const {
+    setProducts, products, setLoading, loading,
+  } = useContext(appContext);
+
+  useEffect(() => {
+    const getApiProducts = async () => {
+      setLoading(true);
+      const response = await getProducts();
+      setProducts(response.data);
+      setLoading(false);
+    };
+    getApiProducts();
+  }, []);
+
+  const searchByName = async ({ target }) => {
+    setLoading(true);
+    const response = await getProducts(target.value);
+    setProducts(response.data);
+    setLoading(false);
+  };
+
   return (
-    <h1>Home</h1>
+    <div className="home">
+      <div className="header">header</div>
+      <div className="search">
+        <input type="text" onChange={searchByName} />
+      </div>
+
+      {loading
+        ? <p>Carregando...</p>
+        : (
+          <div className="cards">
+            {products.map((prod) => (
+              <div key={prod.id}>
+                <h2>{prod.name}</h2>
+                <p>{`R$ ${prod.price}`}</p>
+                <button type="button">Ver produto</button>
+                <button type="button">Adicionar</button>
+              </div>
+            ))}
+          </div>
+        )}
+
+    </div>
   );
 }
 
