3fcdb5f1056c7eb51236e863532aa85cced32793
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-13 20:35:10 -0300

    feat: add camada de model para o metodo getClientById na rota client

--- backend/src/repository/clients/IClientsModel.ts
+++ backend/src/repository/clients/IClientsModel.ts
@@ -1,5 +1,6 @@
-import IClient from "../../interfaces/IClient";
+import IClient, { IClientConsult } from "../../interfaces/IClient";
 
 export default interface IClientsModel {
   create(client:IClient):Promise<void>
+  getClientById(id:number):Promise<IClientConsult | null>
 }
