bc1c12a74d8973d0689cc85f8210051b2bdf8573
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-13 11:03:24 -0300

    feat: adicionado funções de ler e adicionar prod ao carrinho no provider

--- frontend/src/contexts/providerApp.jsx
+++ frontend/src/contexts/providerApp.jsx
@@ -2,16 +2,30 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import appContext from './AppContext';
+import { ReadLocalStoreCart, addLocalStoreCart } from '../services/api/localStore';
 
 function ProviderApp({ children }) {
   const [products, setProducts] = useState();
   const [loading, setLoading] = useState(true);
+  const [cart, setCart] = useState(ReadLocalStoreCart());
+
+  const loadCart = () => {
+    setCart(ReadLocalStoreCart());
+  };
+
+  const addCartProd = (prod) => {
+    addLocalStoreCart(prod);
+    loadCart();
+  };
 
   const context = {
     products,
     setProducts,
     loading,
     setLoading,
+    loadCart,
+    cart,
+    addCartProd,
   };
 
   return (
