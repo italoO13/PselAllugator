e029a5733556c3898022e88face80fcd709c9bbb
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-13 11:29:46 -0300

    feat: quantidade total no carrinho

--- frontend/src/pages/ShoppingCart.jsx
+++ frontend/src/pages/ShoppingCart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Cart from '../components/Cart';
 import appContext from '../contexts/AppContext';
 
@@ -6,10 +6,22 @@ function ShoppingCart() {
   const {
     cart, loadCart, login,
   } = useContext(appContext);
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
     loadCart();
   }, []);
+
+  useEffect(() => {
+    const calculateTotal = () => {
+      let priceTotal = 0;
+      cart.forEach((prod) => {
+        priceTotal += Number(prod.price * prod.qtd);
+      });
+      setTotal(priceTotal);
+    };
+    calculateTotal();
+  }, [cart]);
 
   const load = () => {
     loadCart();
@@ -18,6 +30,7 @@ function ShoppingCart() {
   return (
     <div className="ShoppingCart">
       <h2>Carrinho de compras</h2>
+      <h3>{`Valor Total: R$ ${total}`}</h3>
       <ul>
         {cart.map((prod) => (
           <Cart prod={prod} load={load} />
